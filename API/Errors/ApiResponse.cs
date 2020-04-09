61d59719f2e30c7042b07037e91481848d17abf2
jjcwn11 <jjcwn@verizon.net>
2020-04-09 15:52:21 -0400

    End of Section 5

---
+++ API/Errors/ApiResponse.cs
@@ -0,0 +1,30 @@
+using System;
+
+namespace API.Errors
+{
+    public class ApiResponse
+    {
+        public ApiResponse(int statusCode, string message = null)
+        {
+            this.StatusCode = statusCode;
+            this.Message = message ?? GetDefaultMessageForStatusCode(statusCode);
+
+        }
+
+        private string GetDefaultMessageForStatusCode(int statusCode)
+        {
+            return statusCode switch
+            {
+                400 => "A bad request, you have made",
+                401 => "Authorized, you are not:",
+                404 => " Resource found, itwas not",
+                500 => "Errors. Server Error",
+                _ => null
+            };
+        }
+
+        public int StatusCode { get; set; }
+        public string Message { get; set; }
+
+    }
+}
--- API/Startup.cs
+++ API/Startup.cs
@@ -1,4 +1,6 @@
+using API.Extensions;
 using API.Helpers;
+using API.Middleware;
 using AutoMapper;
 using Core.Interfaces;
 using Infrastructure.Data;
@@ -7,7 +9,6 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Hosting;
 
 namespace API
 {
@@ -24,29 +25,38 @@ namespace API
         {
             services.AddScoped<IProductRepository, ProductRepository>();
             services.AddScoped(typeof(IGenericRepository<>), (typeof(GenericRepository<>)));
+
             services.AddAutoMapper(typeof(MappingProfiles));
             services.AddControllers();
-         
             services.AddDbContext<StoreContext>(x =>
             {
                 x.UseSqlite(_config.GetConnectionString("DefaultConnection"));
             });
-        }
+     
 
+            services.AddApplicationServices();
+            services.AddSwaggerDocumentation();
+
+        }
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
+            // if (env.IsDevelopment())
+            // {
+            //     app.UseDeveloperExceptionPage();
+            // }
+
+
+            app.UseMiddleware<ExceptionMiddleware>();
+            app.UseStatusCodePagesWithReExecute("/errors/{0}"); // redirect to errors controller
 
             app.UseHttpsRedirection();
-
             app.UseRouting();
             app.UseStaticFiles();
 
             app.UseAuthorization();
+            app.UseSwagger();
+            app.UseSwaggerUI(c => {c.SwaggerEndpoint("/swagger/v1/swagger.json", "SkiNet Api v1:");});
 
             app.UseEndpoints(endpoints =>
             {
