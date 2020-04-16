2891cd42105864e6ea601b39fd40cf09b1cb129f
jjcwn11 <jjcwn@verizon.net>
2020-04-16 17:36:20 -0400

    end of section 10

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -2,20 +2,22 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { CoreModule } from './core/core.module';
-import { ShopModule } from './shop/shop.module';
 import { NgModule } from '@angular/core';
-import { HttpClientModule } from '@angular/common/http'; 
+import { HttpClientModule } from '@angular/common/http';
+import { HomeModule } from './home/home/home.module';
+
 
 @NgModule({
   declarations: [
     AppComponent
+    
   ],
   imports: [
     AppRoutingModule,
     BrowserAnimationsModule,
     CoreModule,
-    ShopModule,
-    HttpClientModule
+    HttpClientModule,
+    HomeModule
   ],
   providers: [HttpClientModule],
   bootstrap: [AppComponent]
--- client/src/app/shop/shop.module.ts
+++ client/src/app/shop/shop.module.ts
@@ -2,14 +2,16 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ShopComponent } from './shop.component';
 import { ProductItemComponent } from '../product-item/product-item.component';
+import { ProductDetailsComponent } from './product-details/product-details.component';
 import { SharedModule } from '../shared/shared.module';
+import { ShopRoutingModule } from './shop-routing.module';
 
 @NgModule({
-  declarations: [ShopComponent, ProductItemComponent],
+  declarations: [ShopComponent, ProductItemComponent, ProductDetailsComponent],
   imports: [
     CommonModule,
-    SharedModule
-  ],
-  exports: [ShopComponent]
+    SharedModule,
+    ShopRoutingModule
+  ]
 })
 export class ShopModule { }
